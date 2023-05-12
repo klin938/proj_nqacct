ea6380616af84e69bf5d6411dc3756159de15edf
Derrick Lin <d.lin@garvan.org.au>
2023-05-12 13:22:40 +1000

    Added displayName col in df

--- nqacct/reporting/df_reporter.py
+++ nqacct/reporting/df_reporter.py
@@ -1,3 +1,6 @@
+import nqacct.common.ldap_helper as ldap_helper
+import config.garvan as g
+
 import sys, logging
 import pandas as pd
 
@@ -15,6 +18,16 @@ def init(data):
 def usage_yearly_group_summary(keystr, datecol, valstr):
     this.df['datecol'] = pd.to_datetime(this.df[datecol], dayfirst=True)
     agg = this.df.groupby(by=[this.df.datecol.dt.year, keystr]).agg({valstr:sum})
+    # https://stackoverflow.com/questions/53011418/first-column-in-dataframe-lost-after-grouping
+    # this align all desired index keys back in the same top row
+    agg = agg.reset_index()
+    agg = add_displayname_col(keystr, 'Display Name', agg)
     # transform datecol column to be X-axis and use group/owner as index
-    pivot_tb = pd.pivot_table(agg, values=valstr, index=[keystr], columns=['datecol'], fill_value=0)
+    pivot_tb = pd.pivot_table(agg, values=valstr, index=[keystr,'Display Name'], columns=['datecol'], fill_value=0)
     return pivot_tb
+
+def add_displayname_col(keystr, newcol_key, df):
+    ldap_helper.init(g.LDAP_HOST, g.LDAP_PORT, g.LDAP_BINDDN, g.LDAP_PW, g.LDAP_BASEDN)
+    df[newcol_key] = df[keystr].apply(ldap_helper.get_user_displayname)
+    ldap_helper.close()
+    return df
